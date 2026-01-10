82afa4192014f452ff4aa2637d0b47d4b9a873f0
Dominik Purgał <amonsir29@gmail.com>
2026-01-11 00:17:11 +0100

    - Fix env

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default {
   "expo": {
     "name": "MiniStrava",
     "slug": "MiniStrava",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "scheme": "MiniStrava",
     "platforms": [
       "ios",
